297e28f8f3f20d863aacfd6fbd7229d603ddaeef
Purnima <pooyax14@gmail.com>
2018-03-01 23:25:58 +0530

    remove resetneedAddAnotherbutton
    we no longer are passing resetneedAddAnotherbutton from AddTextarea file

--- form-builder/src/RadioButton/RadioButton.js
+++ form-builder/src/RadioButton/RadioButton.js
@@ -7,7 +7,7 @@ class RadioButton extends Component {
 		this.state={
 			inputTextValue: ""
 		}
-		this.onInputTextChangeValue = this.onInputTextChangeValue.bind(this)
+		this.onInputTextChangeValue = this.onInputTextChangeValue.bind(this);
 	}
 	onInputTextChangeValue(event) {
 		let value = event.target.value;
@@ -16,16 +16,14 @@ class RadioButton extends Component {
 		this.setState({
 			inputTextValue: value,
 		})
-		if(this.props.resetneedAddAnotherbutton)
-	 this.props.resetneedAddAnotherbutton()
-	 this.props.onInputTextUpdate(value,this.props.index,this.props.position)
+	 this.props.onInputTextUpdate(value,this.props.index,this.props.position);
 	}
 	render() {
-		let needAddAnotherbutton = this.props.needAddAnotherbutton
+		let needAddAnotherbutton = this.props.needAddAnotherbutton;
 		return(
 			<div style={{"marginBottom": "22px"}}>
 				<input type="radio" id="contactChoice1" name="contact" value="ischecked"/>
-    			<input type="text" placeholder="Option1" onBlur={(e)=>this.onInputTextChangeValue(e)}/>
+    			<input type="text" placeholder="Option1" defaultValue={this.state.inputTextValue} onBlur={(e)=>this.onInputTextChangeValue(e)}/>
     		{needAddAnotherbutton ? <button className="btn btn-primary btn-sm" 
     		onClick={()=>this.props.onAddButtonClick(this.state.inputTextValue,this.props.index,this.props.position)} >Add Other</button> :""}
     	</div>
