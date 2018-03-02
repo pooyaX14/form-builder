d8fa519e6445e6af6989f5d91e9058640c654e36
Purnima <pooyax14@gmail.com>
2018-03-02 13:36:25 +0530

    remove unused imported files

--- form-builder/src/index.js
+++ form-builder/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import AddQuestion from './AddQuestion/AddQuestion'
-import registerServiceWorker from './registerServiceWorker';
-import bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
-//import ResponsePage from './ResponsePage/ResponsePage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ResponsePageTest from './ResponsePageTest/ResponsePageTest.js';
 
 ReactDOM.render(
@@ -18,27 +14,3 @@ ReactDOM.render(
 		</Switch>
 	</BrowserRouter>, document.getElementById('root')
 )
-// ReactDOM.render(
-// 	<Router history={browserHistory} routes={routes}/>,
-//  	document.getElementById('root')
-// );
-
-
-// registerServiceWorker();
-
-// ReactDOM.render(<AddQuestion />, document.getElementById('root'));
-// registerServiceWorker();
-
-// /* eslint-disable no-console */
-// import React from 'react';
-// import 'babel-polyfill';
-// import {render} from 'react-dom';
-// import { Router, browserHistory } from 'react-router';
-// import routes from './routes';
-// import './styles/style.css'; // webpack can css files too
-// import '../node_modules/bootstrap/dist/css/bootstrap.css';
-
-// render(
-// 	<Router history={browserHistory} routes={routes} />,
-// 	document.getElementById('app')
-// );
